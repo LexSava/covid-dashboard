a19cf4fe835256e267661c8fcea9bd8f45965fad
LexSava <lexsava@gmail.com>
2020-12-19 16:57:11 +0300

    feat: add map
    Add map.

--- covid-dashboard/src/js/main.js
+++ covid-dashboard/src/js/main.js
@@ -3,12 +3,6 @@ import './widget.zero';
 import '../styles/css/style.css';
 import '../styles/sass/style.scss';
 import '../styles/css/fonts.css';
-// import '../styles/css/Chart.min.css';
-// import './Chart.min';
 import './map-script';
 import '../styles/css/map-style.css'
-
-
-import '../styles/css/map-style.css';
-
 import './widget.four';
