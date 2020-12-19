6e946f598f618e7f01c02c818d9d83c5bfee1e6d
LexSava <lexsava@gmail.com>
2020-12-19 12:44:10 +0300

    fix: fix code
    fix code

--- covid-dashboard/src/js/main.js
+++ covid-dashboard/src/js/main.js
@@ -4,5 +4,6 @@ import './wdget.four';
 import '../styles/css/style.css';
 import '../styles/sass/style.scss';
 import '../styles/css/fonts.css';
-// import '../styles/css/Chart.min.css';
-// import './Chart.min';
+
+
+
