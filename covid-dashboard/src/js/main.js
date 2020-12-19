ce93a59f56be31546f3cefe5d5ab82e990cd2f75
LexSava <lexsava@gmail.com>
2020-12-19 13:05:23 +0300

    fix: fix code
    fix code

--- covid-dashboard/src/js/main.js
+++ covid-dashboard/src/js/main.js
@@ -1,6 +1,6 @@
 import './babel';
 import './widget.zero';
-import './wdget.four';
+import './widget.four';
 import '../styles/css/style.css';
 import '../styles/sass/style.scss';
 import '../styles/css/fonts.css';
@@ -10,4 +10,5 @@ import './map-script';
 import '../styles/css/map-style.css'
 
 
+import '../styles/css/map-style.css';
 
