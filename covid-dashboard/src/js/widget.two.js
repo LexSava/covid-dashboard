e9671d6fdaccca093b0109a13b304312fc4fad29
LexSava <lexsava@gmail.com>
2020-12-22 12:52:23 +0300

    feat: added search by country
    Added search by country.

--- covid-dashboard/src/js/widget.two.js
+++ covid-dashboard/src/js/widget.two.js
@@ -3,7 +3,7 @@ const COUNTRY_BLOCK = document.querySelector('.widget__two__block__countries')
 const CASES_DATA = document.querySelector('.cases')
 const DEATHS_DATA = document.querySelector('.deaths')
 const RECOVERED_DATA = document.querySelector('.recovered')
-
+let results;
 
 function CreatesListCountries(flag, country, indicator) {
     const newСountryСontainer = document.createElement('div');
@@ -27,18 +27,6 @@ function CreatesListCountries(flag, country, indicator) {
     newСountryСontainer.appendChild(newIndicatorCountry);
 
 }
-
-
-// const getRegetResultsOnCovid = async () => {
-//     const response = await fetch('https://disease.sh/v2/countries');
-//     const results = await response.json();
-//     return results;
-// }
-
-// console.log(getRegetResultsOnCovid());
-
-
-let results;
 
 async function GetCountryData(val) {
     const response = await fetch('https://disease.sh/v2/countries');
@@ -90,3 +78,58 @@ DEATHS_DATA.addEventListener("click", () => GetCountryData(2));
 RECOVERED_DATA.addEventListener("click", () => GetCountryData(3));
 
 GetCountryData(1);
+
+
+const search = document.querySelector('#search');
+
+const searchStates = async searchText => {
+    const response = await fetch('https://disease.sh/v2/countries');
+    const dataObtained = await response.json();
+    console.log(dataObtained);
+    let matches = dataObtained.filter(state => {
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        return state.country.match(regex);
+    });
+    if (searchText.length === 0) {
+        matches = [];
+    }
+    outputHtml(matches);
+    console.log(matches);
+}
+const outputHtml = matches => {
+    while (COUNTRY_BLOCK.firstChild) {
+        COUNTRY_BLOCK.removeChild(COUNTRY_BLOCK.firstChild);
+    }
+    if (matches.length > 0) {
+        const html = matches.map(match => {
+
+            const newСountryСontainer = document.createElement('div');
+            newСountryСontainer.classList.add('widget__two__countries');
+            COUNTRY_BLOCK.appendChild(newСountryСontainer);
+
+            const newFlagImage = document.createElement('img');
+            newFlagImage.classList.add('widget__two__flag');
+            newFlagImage.src = `${match.countryInfo.flag}`;
+
+            const newNameCountry = document.createElement('span');
+            newNameCountry.classList.add('widget__two__name__countries');
+            newNameCountry.innerHTML = `${match.country}`;
+
+            const newIndicatorCountry = document.createElement('span');
+            newIndicatorCountry.classList.add('widget__two__indicator__countries');
+            newIndicatorCountry.innerHTML = ` - ${match.cases}`;
+
+            newСountryСontainer.appendChild(newFlagImage);
+            newСountryСontainer.appendChild(newNameCountry);
+            newСountryСontainer.appendChild(newIndicatorCountry);
+        }
+        );
+    }
+};
+
+
+search.addEventListener('input', () => searchStates(search.value))
+
+
+
+
