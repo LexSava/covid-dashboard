30e3460d13d8f962a7c70f516849decab9c6dac8
Mikhail Mudrogelov <mudrogelov.beled@gmail.com>
2020-12-23 00:38:58 +0300

    feat: implement button change category

--- covid-dashboard/src/js/widget-table.js
+++ covid-dashboard/src/js/widget-table.js
@@ -18,6 +18,8 @@
 // }
 
 // getCountries();
+const categoryButtonPeriod = document.querySelector('.category-name_period');
+const categoryButtonValue = document.querySelector('.category-name_value');
 
 class Table {
   constructor(url) {
@@ -34,34 +36,98 @@ class Table {
     this.getData(content);
   }
 
+  // if (this.period === 'all') {
+  //   if (this.value === 'perOneHundredThousand') {
+  //     this.cases = content.casesPerOneMillion * 10;
+  //     this.deaths = content.deathsPerOneMillion * 10;
+  //     this.recovered = content.recoveredPerOneMillion * 10; 
+  //   } else if (this.value === 'absolute') {
+  //     this.cases = content.cases;
+  //     this.deaths = content.deaths;
+  //     this.recovered = content.recovered; 
+  //   }
+  // } else if (this.period === 'today') {
+  //   this.cases = content.todayCases;
+  //   this.deaths = content.todayDeaths;
+  //   this.recovered = content.todayRecovered;
+  // }
+
   getData(content) {
-    if (this.period === 'all') {
-      if (this.value === 'perOneHundredThousand') {
-        this.diseases = content.casesPerOneMillion * 10;
-        this.deaths = content.deathsPerOneMillion * 10;
-        this.recovered = content.recoveredPerOneMillion * 10; 
-      } else if (this.value === 'absolute') {
-        this.diseases = content.cases;
-        this.deaths = content.deaths;
-        this.recovered = content.recovered; 
-      }
-    } else if (this.period === 'today') {
-      this.diseases = content.todayCases;
-      this.deaths = content.todayDeaths;
-      this.recovered = content.todayRecovered;
+    switch (this.period) {
+      case 'all':
+        switch (this.value) {
+          case 'absolute':
+            this.cases = content.cases;
+            this.deaths = content.deaths;
+            this.recovered = content.recovered; 
+          break;
+
+          case 'perOneHundredThousand':
+            this.cases = Math.round(content.casesPerOneMillion * 10);
+            this.deaths = Math.round(content.deathsPerOneMillion * 10);
+            this.recovered = Math.round(content.recoveredPerOneMillion * 10); 
+          break;
+        }
+      break;
+      case 'today':
+        switch (this.value) {
+          case 'absolute':
+            this.cases = content.todayCases;
+            this.deaths = content.todayDeaths;
+            this.recovered = content.todayRecovered; 
+          break;
+
+          case 'perOneHundredThousand':
+            this.cases = Math.round(content.todayCases);
+            this.deaths = Math.round(content.todayDeaths);
+            this.recovered = Math.round(content.todayRecovered); 
+          break;
+        }
+      break;
     }
 
     this.updateTextElements();
   }
 
   updateTextElements() {
-    document.querySelector('.count_diseases').textContent = this.diseases;
+    document.querySelector('.count_cases').textContent = this.cases;
     document.querySelector('.count_deaths').textContent = this.deaths;
     document.querySelector('.count_recovered').textContent = this.recovered;
   }
-  
+
+  changeCategoryPeriod() {
+    if (categoryButtonPeriod.textContent === 'All period') {
+      this.period = 'today';
+      categoryButtonPeriod.textContent = 'Last day';
+    } else {
+      this.period = 'all';
+      categoryButtonPeriod.textContent = 'All period';
+    }
+  }
+
+  changeCategoryValue() {
+    if (categoryButtonValue.textContent === 'Absolute') {
+      this.value = 'perOneHundredThousand';
+      categoryButtonValue.textContent = 'Per 100.000';
+    } else {
+      this.value = 'absolute';
+      categoryButtonValue.textContent = 'Absolute';
+    } 
+  }
 }
 
 const url = 'https://disease.sh/v3/covid-19/all'
 const table = new Table(url);
 table.getRequest();
+
+categoryButtonPeriod.addEventListener('click', () => {
+  table.changeCategoryPeriod();
+  table.getRequest();
+  console.log('Period...');
+});
+
+categoryButtonValue.addEventListener('click', () => {
+  table.changeCategoryValue();
+  table.getRequest();
+  console.log('Value...')
+});
